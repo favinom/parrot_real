e1006bb8efa1b2167d951f7d3c033d6a55598f2f
Marco Favino <favinom@usi.ch>
2019-02-07 14:44:22 +0100

    added porosity timederivative

---
+++ include/kernels/PorosityTimeDerivative.h
@@ -0,0 +1,40 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef POROSITYTIMEDERIVATIVE_H
+#define POROSITYTIMEDERIVATIVE_H
+
+#include "TimeDerivative.h"
+
+// Forward Declarations
+class PorosityTimeDerivative;
+
+/**
+ * validParams returns the parameters that this Kernel accepts / needs
+ * The actual body of the function MUST be in the .C file.
+ */
+template <>
+InputParameters validParams<PorosityTimeDerivative>();
+
+class PorosityTimeDerivative : public TimeDerivative
+{
+public:
+  PorosityTimeDerivative(const InputParameters & parameters);
+
+protected:
+  virtual Real computeQpResidual();
+  virtual Real computeQpJacobian();
+
+  /**
+   * This MooseArray will hold the reference we need to our
+   * material property from the Material class
+   */
+  const MaterialProperty<Real> &_phi;
+};
+#endif // COEFTIMEDERIVATIVE_H
---
+++ src/kernels/PorosityTimeDerivative.C
@@ -0,0 +1,39 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "PorosityTimeDerivative.h"
+
+registerMooseObject("MooseApp", PorosityTimeDerivative);
+
+template <>
+InputParameters
+validParams<PorosityTimeDerivative>()
+{
+  InputParameters params = validParams<TimeDerivative>();
+  return params;
+}
+
+PorosityTimeDerivative::PorosityTimeDerivative(const InputParameters & parameters) :
+TimeDerivative(parameters),
+_phi(getMaterialProperty<Real>("Porosity"))
+{}
+
+Real
+PorosityTimeDerivative::computeQpResidual()
+{
+  // We're reusing the TimeDerivative Kernel's residual
+  // so that we don't have to recode that.
+  return _phi[_qp] * TimeDerivative::computeQpResidual();
+}
+
+Real
+PorosityTimeDerivative::computeQpJacobian()
+{
+  return _phi[_qp] * TimeDerivative::computeQpJacobian();
+}
