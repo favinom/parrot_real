3f1bfad824744178369d87490803d15bf478628b
Marco Favino <favinom@usi.ch>
2019-02-07 14:26:28 +0100

    changed lines

---
+++ include/materials/Porosity.h
@@ -0,0 +1,33 @@
+#ifndef POROSITY_H
+#define POROSITY_H
+
+#include "Material.h"
+
+class Porosity;
+
+template <>
+InputParameters validParams<Porosity>();
+
+/**
+ * Material for supplying a conductivity tensor.
+ * \f$G=\sum_{i=f,s,n}\sigma_i(\vec{e}_i\otimes\vec{e}_i)\f$
+ * Conductivities in fibre, sheet and sheet-normal directions are read from the
+ * input file.
+ * For sensible values, see e.g. \ref Potse2006 "Potse, 2006, Table I".
+ */
+class Porosity : public Material
+{
+public:
+  Porosity(const InputParameters &parameters);
+
+protected:
+  virtual void computeQpProperties();
+    
+    protected:
+    
+  Real _phiInput;
+  MaterialProperty<Real> &_phi;
+
+};
+
+#endif
---
+++ src/materials/Porosity.C
@@ -0,0 +1,29 @@
+#include "Porosity.h"
+
+// MOOSE includes
+//#include "Assembly.h"
+//#include "MooseMesh.h"
+
+registerMooseObject("parrot_realApp", Porosity);
+
+template <>
+InputParameters
+validParams<Porosity>()
+{
+  InputParameters params = validParams<Material>();
+  params.addRequiredParam<Real>("phi","phi");
+
+  return params;
+}
+
+Porosity::Porosity(const InputParameters &parameters) :
+Material(parameters),
+_phiInput(getParam<Real>("phi")),
+_phi(declareProperty<Real>("Porosity"))
+{}
+
+void
+Porosity::computeQpProperties()
+{
+    _phi[_qp]=_phiInput;
+}
